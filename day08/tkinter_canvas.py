fa6a3107eec8fb77ad19388d75bfe57236133e0a
0l1xscr1pt <scr1ptkyu01@gmail.com>
2023-09-20 15:53:01 +0200

    delete \n

--- day08/tkinter_canvas.py
+++ day08/tkinter_canvas.py
@@ -8,7 +8,6 @@ root.geometry("500x500")
 monCanvas = Canvas(root, width=500, height=500, bg='ivory')
 
 img = ImageTk.PhotoImage(Image.open("/day08/parrot.png"))
-
 monCanvas.create_image(10, 10, anchor = NW, image = img)
 
 monCanvas.pack()
