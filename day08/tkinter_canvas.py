9726ea1f9c4b20478978b5f1ac560c03b77e78eb
0l1xscr1pt <scr1ptkyu01@gmail.com>
2023-09-20 15:50:48 +0200

    add thinker canvas

--- day08/tkinter_canvas.py
+++ day08/tkinter_canvas.py
@@ -7,16 +7,9 @@ root.geometry("500x500")
 
 monCanvas = Canvas(root, width=500, height=500, bg='ivory')
 
-# img= Image.PhotoImage(Image.open("parrot.png"))
-# canvas.create_image(10,10,anchor=NW,image=img)
-
-
-img = ImageTk.PhotoImage(Image.open("/home/kali/epitech/pythonPrePool/day08/parrot.png"))
+img = ImageTk.PhotoImage(Image.open("/day08/parrot.png"))
 
 monCanvas.create_image(10, 10, anchor = NW, image = img)
-
-
-
 
 monCanvas.pack()
 root.mainloop()
